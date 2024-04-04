bb1f228401eb7c27b16cdafbc5bbd3c05e92c765
h581296 <581296@stud.hvl.no>
2024-04-04 13:30:57 +0200

    augh1

--- Oblig4/src/oppgave1/Bilskilt.java
+++ Oblig4/src/oppgave1/Bilskilt.java
@@ -1,20 +1,35 @@
 package oppgave1;
 
-import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.Set;
 
 public class Bilskilt {
     //
 
-    private static final Set<String> BILSKILT = Set.of("EL65431, TA14374, ZX87181, EL47007, VV50000, UV14544, EL32944");
-    private static HashSet<String> hasj = HashSet.newHashSet(10);
-
+    private static Hashtable<Integer, String> bilHasj = new Hashtable<>();
+    private static String[] bilSkilt = {"EL65431", "TA14374", "ZX87181", "EL47007", "VV50000", "UV14544", "EL32944"};
     public static void main(String[] args) {
-        hasj.add(BILSKILT.stream().toString());
+        for (String s : bilSkilt) {
+            int indx = hash(s);
+            if (bilHasj.get(indx) == null) {
+                bilHasj.put(indx, s);
+            } else {
+                while (bilHasj.get(indx + 1) != null) {
+                    indx++;
+                }
+                bilHasj.put(indx + 1, s);
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            if (bilHasj.get(i) != null) {
+                System.out.println(bilHasj.get(i));
+            }
+        }
     }
-
-
-
+    public static int hash(String string) {
+        return Integer.parseInt(String.valueOf(string.charAt(string.length() - 1))) ;
+    }
 }
 
 
