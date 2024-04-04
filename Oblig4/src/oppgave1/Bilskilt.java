8441b44f53db6ca1c1d40a4efe7bb74747a1a7a9
h581296 <581296@stud.hvl.no>
2024-04-04 12:34:47 +0200

    augh1

--- Oblig4/src/oppgave1/Bilskilt.java
+++ Oblig4/src/oppgave1/Bilskilt.java
@@ -1,16 +1,20 @@
 package oppgave1;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class Bilskilt {
     //
 
-    private HashSet<String> hasj = HashSet.newHashSet(10);
+    private static final Set<String> BILSKILT = Set.of("EL65431, TA14374, ZX87181, EL47007, VV50000, UV14544, EL32944");
+    private static HashSet<String> hasj = HashSet.newHashSet(10);
 
-
-    private enum BilSkilt {
-        EL65431, TA14374, ZX87181, EL47007, VV50000, UV14544, EL32944
+    public static void main(String[] args) {
+        hasj.add(BILSKILT.stream().toString());
     }
+
+
+
 }
 
 
