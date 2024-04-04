5d2b6f371e107a6cde68b63029dfd2a0413f3993
h581296 <581296@stud.hvl.no>
2024-04-04 12:24:19 +0200

    augh1

--- Oblig4/src/oppgave1/Bilskilt.java
+++ Oblig4/src/oppgave1/Bilskilt.java
@@ -1,6 +1,16 @@
 package oppgave1;
 
+import java.util.HashSet;
+
 public class Bilskilt {
     //
 
+    private HashSet<String> hasj = HashSet.newHashSet(10);
+
+
+    private enum BilSkilt {
+        EL65431, TA14374, ZX87181, EL47007, VV50000, UV14544, EL32944
+    }
 }
+
+
