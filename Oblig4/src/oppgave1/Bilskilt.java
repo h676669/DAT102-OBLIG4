9a1a3994edf3a2e449ddf9c7f065713426acfb0c
h581296 <581296@stud.hvl.no>
2024-04-04 14:14:02 +0200

    b ?

--- Oblig4/src/oppgave1/Bilskilt.java
+++ Oblig4/src/oppgave1/Bilskilt.java
@@ -2,14 +2,16 @@ package oppgave1;
 
 import java.util.Hashtable;
 import java.util.LinkedList;
-import java.util.Set;
 
 public class Bilskilt {
-    //
 
-    private static Hashtable<Integer, String> bilHasj = new Hashtable<>();
     private static String[] bilSkilt = {"EL65431", "TA14374", "ZX87181", "EL47007", "VV50000", "UV14544", "EL32944"};
     public static void main(String[] args) {
+    }
+
+
+    private static void openAdrLinProbe() {
+        Hashtable<Integer, String> bilHasj = new Hashtable<>(10);
         for (String s : bilSkilt) {
             int indx = hash(s);
             if (bilHasj.get(indx) == null) {
@@ -22,11 +24,22 @@ public class Bilskilt {
             }
         }
         for (int i = 0; i < 10; i++) {
-            if (bilHasj.get(i) != null) {
-                System.out.println(bilHasj.get(i));
-            }
+            System.out.println(bilHasj.get(i));
         }
     }
+
+    private static void kjedetListe() {
+        Hashtable<Integer, LinkedList<String>> bilLink = new Hashtable<>();
+        for (String s : bilSkilt) {
+            int indx = hash(s);
+            if (bilLink.get(indx) == null) {
+                bilLink.put(indx, new LinkedList<>());
+                bilLink.get(indx).add(s);
+            }
+            bilLink.get(indx).add(s);
+        }
+    }
+
     public static int hash(String string) {
         return Integer.parseInt(String.valueOf(string.charAt(string.length() - 1))) ;
     }
