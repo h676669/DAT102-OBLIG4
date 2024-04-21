08526672ec71e242b7d7cbaaa587a0363fc50a6d
h581296 <581296@stud.hvl.no>
2024-04-21 04:41:58 +0200

    selection/insertion sort 100% utestet

--- Oblig4/src/uke16_oppgave1/InsertionSort.java
+++ Oblig4/src/uke16_oppgave1/InsertionSort.java
@@ -1,27 +1,40 @@
 package uke16_oppgave1;
 
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class InsertionSort<T extends Comparable<? super T>> {
 
-    public List<T> finMin(T[] arr, int n) {
-        aughSort(arr, n);
-        return new ArrayList<>(Arrays.asList(arr).subList(0, n));
+    public List<T> finMin(T[] array, int n) {
+        insertionSort(array, n);
+        return new ArrayList<>(Arrays.asList(array).subList(0, n));
     }
 
-    public void aughSort(T[] arr, int c) {
-            for (int x = 0; x < arr.length; x++) {
-                T key = arr[x];
-                int j = x - 1;
-                while (j >= 0 && key.compareTo(arr[j]) < 0) {
-                    arr[j + 1] = arr[j];
-                    j = j - 1;
-                }
-                arr[j + 1] = key;
+    public static <T extends Comparable<? super T>> void insertionSort(T[] array, int n) {
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j >= 1 && array[j - 1].compareTo(array[j]) > 0) {
+                T temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
+                j--;
             }
         }
+        for (int i  = n + 1; i < array.length; i++) {
+            if (array[i].compareTo(array[n]) < 0) {
+                T temp = array[i];
+                array[i] = array[n];
+                array[n] = temp;
+                int j = n;
+                while (j >= 1 && array[j - 1].compareTo(array[j]) > 0) {
+                    temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                    j--;
+                }
+            }
+        }
+    }
 
 }
